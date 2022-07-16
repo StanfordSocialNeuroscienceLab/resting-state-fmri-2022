c3436755e7f7c7a9aae6b9b8918f8b90076eec2f
Ian <47256454+IanRFerguson@users.noreply.github.com>
2022-07-15 17:03:53 -0700

    external save function
    Native `save_matrix` logic causing downstream pickling issues ... this corrects that problem as a placeholder for a patch in GLMX

--- utility/fit_matrices.py
+++ utility/fit_matrices.py
@@ -12,33 +12,47 @@ Ian Richard Ferguson | Stanford University
 """
 
 # --- Imports
-import sys
+import sys, os
 from bids import BIDSLayout
 from glm_express import RestingState
 from tqdm import tqdm
+import numpy as np
 
 # --- Definitions
 def main():
 
+      # Rel path to BIDS root
       bids_path = sys.argv[1]
 
+      # Instantiate BIDSLayout
       bids = BIDSLayout(bids_path)
 
+      # Open error log
       with open("./resting_state.txt", "w") as log:
 
             log.write("\t=== Resting State fMRI Analysis: Connectivity Mapping ===\n\n")
 
+            # Loop through BIDS subjects
             for sub in tqdm(bids.get_subjects()):
 
+                  # Instantiate RestingState object
                   temp_sub = RestingState(sub_id=sub,
                                           suppress=True,
                                           bids_root=bids_path)
 
+                  # Extract and save matrix
                   try:
-                        temp_sub.matrix_from_masker(verbose=False, show_plots=False,
-                                                    save_matrix_output=True,
-                                                    save_plots=True)
+                        mask_matrix = temp_sub.matrix_from_masker(verbose=False, 
+                                                                  show_plots=False,
+                                                                  save_matrix_output=False,
+                                                                  save_plots=True)
 
+                        output_path = os.path.join(temp_sub.first_level_output, "models", f"sub-{sub}_task-rest_aggregated.npy")
+
+                        with open(output_path, "wb") as out:
+                              np.save(out, mask_matrix)
+
+                  # Catch matrix extraction error
                   except Exception as e:
                         log.write(f"\nsub-{sub}:\t\t{e}\n")
 
